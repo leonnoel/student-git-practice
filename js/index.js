9911c49ba7fe1b70cefc7470d1f251a563aa61a0
Blandaxt <resilient@resilients-MacBook-Pro-2.local>
2018-08-14 17:30:06 -0400

    added a return and modified my code to be a function expression and added a log

--- js/index.js
+++ js/index.js
@@ -26,13 +26,16 @@ function displayCompleteMessage(msg){
 
 
 // i set the math .random method to the variale computerChoice
+let computer = function ()
+{
+
 let computerChoice = Math.random();
 
 //  i am creating an if condition that will determine what symbol (rock, paper, scissors) the computer will choose.
 if(computerChoice <= 0.33)
 // if computerChoice is les than or equal to 0.33 in math.random, then it is rock
 {
-  computer = "Rock"
+  computerChoice = "Rock"
 
 }else if (computerChoice > 0.33 && computerChoice <= 0.66)
 // if the computerChoice is greater than .33 and less than or equal to .66 then it is paper
@@ -43,4 +46,10 @@ if(computerChoice <= 0.33)
 // anything else is paper
 {
   computerChoice = "Scissors"
+}
+
+console.log(computerChoice);
+
+return computerChoice;
+
 }
